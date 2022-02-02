034e992704e60ae4978336b01e62f15cb97699b6
Sylvain <dev.tess.sylvain@gmail.com>
2022-02-02 15:16:45 +0100

    upload image profile

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -1,4 +1,5 @@
 <template>
+  <!--  //Déconexion-->
   <div class="card">
 
     <div class="form-row">
@@ -10,17 +11,39 @@
     <h1 class="card__title">{{ user.firstName }}</h1>
     <p class="card__subtitle">Voilà donc qui je suis...</p>
 
+    <!--// L'image de PROFILE-->
     <div class="card__picture-container">
-      <img class="card__picture-img" :src="user.picture" alt="Image de profile">
-      <v-btn class="card__picture-btn ma-2"
+      <img class="card__picture-img" v-if="imageUrl === ''" :src="picture" alt="Image de profile">
+      <img class="card__picture-img" v-else :src="imageUrl" alt="Image de profile valid">
+
+      <v-btn
+          v-if="imageUrl !== ''"
+          @click="validPictureProfile"
+          class="card__picture-btn-valid"
+          color="#1A8917"
+          icon="mdi-check-outline"
+          size="small"
+      ></v-btn>
+
+      <v-btn class="card__picture-btn ma-2" @click="updatePictureProfile"
              text
              icon
+             size="small"
              color="#3A3B3C">
         <v-icon>mdi-camera-flip-outline</v-icon>
       </v-btn>
+      <input type="file"
+             style="display: none"
+             ref="fileInputPirctureProfile"
+             accept="image/png, image/jpeg, image/jpg, image/gif"
+             size="5 * 1024 * 1024"
+             @change="onFilePictureProfile">
     </div>
+
+
+    <!--    //Pour les POSTS-->
     <div class="posts" v-for="post in user.posts" :key="post.id">
-      <div v-if="post == null" >
+      <div v-if="post == null">
         <p> L'utilisateur n'as pas fait de posts </p>
       </div>
       <div class="posts__posts-name">
@@ -50,16 +73,73 @@ export default {
     this.$store.dispatch('getUserInfos', uuidUser);
   },
 
+  data() {
+    return {
+      imageUrl: "",
+      image: null
+    }
+  },
+
+
   methods: {
     logout: function () {
       this.$store.commit('logout')
       this.$router.push("/")
-    }
+    },
+
+    updatePictureProfile: function () {
+      this.$refs.fileInputPirctureProfile.click();
+    },
+    onFilePictureProfile: function (event) {
+      const eventChange = event.target.files
+      const files = eventChange[0]
+      const sizeFile = files.size
+      const fileName = files.name
+
+      if (sizeFile > 5 * 1024 * 1024) {
+        event.preventDefault();
+        alert('File too big (> 5MB)');
+        return;
+      }
+      if (fileName.lastIndexOf('.') <= 0) {
+        return alert('Ajouter une image valide')
+      }
+
+      const fileReader = new FileReader()
+      fileReader.addEventListener('load', () => {
+        this.imageUrl = fileReader.result
+        console.log(this.imageUrl)
+      })
+      fileReader.readAsDataURL(files)
+      this.image = files
+      console.log(this.image)
+    },
+
+    validPictureProfile: function () {
+      if (!this.image) {
+        return;
+      }
+
+      const formData = new FormData()
+      formData.append('profile', this.image)
+      console.log(formData.get('profile'))
+
+      const payload = {
+        uuidUser: this.$store.state.user.uuidUser,
+        picture: formData
+
+      }
+      this.$store.dispatch('updatePictureProfile', payload)
+      this.image = null
+      this.imageUrl = ""
+    },
+
   },
 
   computed: {
     ...mapState({
-      user: "userInfos"
+      user: "userInfos",
+      picture : "pictureProfile"
     }),
   }
 }
@@ -90,9 +170,15 @@ export default {
       right: 0;
       bottom: 1%;
     }
+
+    .card__picture-btn-valid {
+      position: absolute;
+      right: 75%;
+      bottom: 6%;
+    }
   }
 
-  .posts{
+  .posts {
     height: 100px;
   }
 
